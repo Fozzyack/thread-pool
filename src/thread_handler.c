7727f8eb070e5ae111e27ea9603f9c39bb5f9017
Frasier <fsundra@gmail.com>
2026-08-28 01:56:52 +0800

    [main] init returns on fail

--- src/thread_handler.c
+++ src/thread_handler.c
@@ -16,10 +16,12 @@ thread_handler_status init_thread_handler(thread_handler_threadpool *pool) {
   if ((status = pthread_mutex_init(&(pool->lock), NULL)) != 0) {
     fprintf(stderr, "Error: pthread_mutex_init - status: %d err:%s\n", status,
             strerror(status));
+    return THREAD_HANDLER_ERROR;
   }
   if ((status = pthread_cond_init(&(pool->condition), NULL)) != 0) {
     fprintf(stderr, "Error: pthread_mutex_init - status: %d err:%s\n", status,
             strerror(status));
+    return THREAD_HANDLER_ERROR;
   }
 
   return THREAD_HANDLER_OK;
