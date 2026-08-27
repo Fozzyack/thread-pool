0448a9639358d26593c804d4a5e135b071a2a3c8
Frasier <fsundra@gmail.com>
2026-08-28 02:06:38 +0800

    [main] adds thread_handler init function to main

--- src/main.c
+++ src/main.c
@@ -3,6 +3,13 @@
 
 #include "thread_handler.h"
 int main(void) {
-    printf("Hello World\n");
+    thread_handler_threadpool thread_pool;
+
+    thread_handler_status status = init_thread_handler(&thread_pool);
+    if (status == THREAD_HANDLER_ERROR) {
+        fprintf(stderr, "Error: init_thread_handler\n");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
