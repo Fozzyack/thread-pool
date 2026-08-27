a725ac7f9fbeb6c7cef30be4f65c759392d31744
Frasier <fsundra@gmail.com>
2026-08-28 01:50:11 +0800

    [main] adds init for queue and mutex

--- src/thread_handler.c
+++ src/thread_handler.c
@@ -3,4 +3,12 @@
 #include "thread_handler.h"
 #include <pthread.h>
 
-thread_handler_status init_thread_handler(thread_handler_threadpool *pool) {}
+thread_handler_status init_thread_handler(thread_handler_threadpool *pool) {
+
+  // init queue
+  pool->queue_count = 0;
+  pool->queue_front = 0;
+  pool->queue_back = 0;
+
+  pthread_mutex_init(pool->lock, NULL);
+}
